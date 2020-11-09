9a8ee7f2cb3cbe888eb8478ab61b43e003a6d106
Ali Hosni <ali-hosni.bouslimi@easyflyer.fr>
2020-11-09 15:24:31 +0100

    update rules

--- rules/index.js
+++ rules/index.js
@@ -1,33 +1,17 @@
 module.exports = {
   env: {
+    commonjs: true,
     es2021: true,
-    jest: true,
-    mocha: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'airbnb-base', 'prettier'
+  ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
   },
-  plugins: ["prettier", "@typescript-eslint"],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": ["error", { singleQuote: false }],
-    "no-new": "off",
-    "no-restricted-syntax": "off",
-    "class-methods-use-this": "off",
-    "no-await-in-loop": "off",
-    "no-async-promise-executor": "off",
-    "no-underscore-dangle": "off",
-    "no-console": [
-      "error",
-      {
-        "allow": [
-          "error",
-          "log"
-        ]
-      }
-    ],
-  }
+    'prettier/prettier': ['error', { singleQuote: true }]
+  },
 };
