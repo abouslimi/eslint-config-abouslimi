b97ba5ac7936f480836f4d3b458100c2af4e9368
Ali Hosni <ali-hosni.bouslimi@easyflyer.fr>
2020-11-10 23:53:22 +0100

    add new rules

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: ["eslint:recommended", "google"],
   rules: {
     "quotes": ["error", "double", { allowTemplateLiterals: true }],
+    "no-async-promise-executor": "off",
+    "no-return-await": "off",
     "object-curly-spacing": "off",
+    "no-new-object": "error",
     "no-undef": "off",
   },
 };
